c9186e416fb39234ee31448422750c143d378190
jordantriportage <jdiochotwork@gmail.com>
2026-02-16 14:39:48 +0100

    bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ from telegram.ext import (
     filters,
 )
 
-TOKEN = "7244281986:AAF_ojoZVFL6fG41j01ckDcDsBGYJ592c6Q"
+TOKEN = "7244281986:AAHyQE7rMPElsW77a1LuSrti9ROVXlbCY_M"
 
 GROUP_CHAT_ID = -5156847371  # 🔴 ID du groupe
 
@@ -174,4 +174,5 @@ app.add_handler(CallbackQueryHandler(button_handler))
 
 if __name__ == "__main__":
     app.run_polling()
+
 
